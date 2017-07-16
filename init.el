e59ed527fff71b29bbafe4964b525acfa239dadf
Tagir Magomedov <tagir@uber.com>
2017-07-16 23:24:37 +0200

    Set lines. Set color mode. Install Groovy & Haskell modes and package manager.

---
+++ init.el
@@ -0,0 +1,27 @@
+; set line numbering and enable it globally
+(setq linum-format "%d ")
+(global-linum-mode 1)
+
+; set color theme
+(load-theme 'wombat)
+
+; provide package repositories for modes
+(require 'package)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-archives
+   (quote
+    (("gnu" . "http://elpa.gnu.org/packages/")
+     ("melpa-stable" . "http://stable.melpa.org/packages/"))))
+ '(package-selected-packages (quote (groovy-mode haskell-mode))))
+(package-initialize)
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
