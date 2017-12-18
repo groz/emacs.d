3780cc90a7dea3399182069531c31492a7c84b4a
Tagir Magomedov <tagir@uber.com>
2017-12-18 21:51:31 +0100

    Install Elpy for python development

--- init.el
+++ init.el
@@ -1,27 +1,64 @@
-; set line numbering and enable it globally
-(setq linum-format "%d ")
-(global-linum-mode 1)
+;; require new line at end of file
+(setq require-final-newline 't)
 
-; set color theme
+;; set line numbering and enable it globally
+(setq linum-format "%d ")
+(global-linum-mode t)
+
+;; set color theme
 (load-theme 'wombat)
 
-; provide package repositories for modes
+;; provide package repositories for modes
 (require 'package)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default bold shadow italic underline bold bold-italic bold])
+ '(ansi-color-names-vector
+   (vector "#212121" "#B71C1C" "#558b2f" "#FFA000" "#2196f3" "#4527A0" "#00796b" "#FAFAFA"))
+ '(custom-enabled-themes (quote (wombat)))
+ '(custom-safe-themes
+   (quote
+    ("b97a01622103266c1a26a032567e02d920b2c697ff69d40b7d9956821ab666cc" "a24c5b3c12d147da6cef80938dca1223b7c7f70f2f382b26308eba014dc4833a" "732b807b0543855541743429c9979ebfb363e27ec91e82f463c91e68c772f6e3" default)))
+ '(fci-rule-color "#dadada")
+ '(hl-sexp-background-color "#efebe9")
  '(package-archives
    (quote
     (("gnu" . "http://elpa.gnu.org/packages/")
      ("melpa-stable" . "http://stable.melpa.org/packages/"))))
- '(package-selected-packages (quote (groovy-mode haskell-mode))))
+ '(package-selected-packages
+   (quote
+    (elpy material-theme dracula-theme markdown-mode neotree groovy-mode haskell-mode)))
+ '(vc-annotate-background nil)
+ '(vc-annotate-color-map
+   (quote
+    ((20 . "#B71C1C")
+     (40 . "#FF5722")
+     (60 . "#FFA000")
+     (80 . "#558b2f")
+     (100 . "#00796b")
+     (120 . "#2196f3")
+     (140 . "#4527A0")
+     (160 . "#B71C1C")
+     (180 . "#FF5722")
+     (200 . "#FFA000")
+     (220 . "#558b2f")
+     (240 . "#00796b")
+     (260 . "#2196f3")
+     (280 . "#4527A0")
+     (300 . "#B71C1C")
+     (320 . "#FF5722")
+     (340 . "#FFA000")
+     (360 . "#558b2f"))))
+ '(vc-annotate-very-old-color nil))
 (package-initialize)
 
 ;; refresh & install selected packages
-(package-refresh-contents)
-(package-install-selected-packages)
+; (package-refresh-contents)
+; (package-install-selected-packages)
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -29,3 +66,11 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+;; bind neotree
+(global-set-key [f8] 'neotree-toggle)
+(setq neo-smart-open t)
+(setq neo-autorefresh t)
+
+;; remove all trailing whitespace
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
