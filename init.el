90582b65040d9b981f63d149256109732eb5bfcb
Tagir Magomedov <tagir@uber.com>
2018-09-27 14:05:11 +0200

    Update settings

--- init.el
+++ init.el
@@ -6,7 +6,7 @@
 (global-linum-mode t)
 
 ;; set color theme
-(load-theme 'wombat)
+; (load-theme 'wombat)
 
 ;; provide package repositories for modes
 (require 'package)
@@ -19,10 +19,10 @@
    [default bold shadow italic underline bold bold-italic bold])
  '(ansi-color-names-vector
    (vector "#212121" "#B71C1C" "#558b2f" "#FFA000" "#2196f3" "#4527A0" "#00796b" "#FAFAFA"))
- '(custom-enabled-themes (quote (wombat)))
+ '(custom-enabled-themes (quote (material)))
  '(custom-safe-themes
    (quote
-    ("b97a01622103266c1a26a032567e02d920b2c697ff69d40b7d9956821ab666cc" "a24c5b3c12d147da6cef80938dca1223b7c7f70f2f382b26308eba014dc4833a" "732b807b0543855541743429c9979ebfb363e27ec91e82f463c91e68c772f6e3" default)))
+    ("9e54a6ac0051987b4296e9276eecc5dfb67fdcd620191ee553f40a9b6d943e78" "7f1263c969f04a8e58f9441f4ba4d7fb1302243355cb9faecb55aec878a06ee9" "1157a4055504672be1df1232bed784ba575c60ab44d8e6c7b3800ae76b42f8bd" "5ee12d8250b0952deefc88814cf0672327d7ee70b16344372db9460e9a0e3ffc" "cf08ae4c26cacce2eebff39d129ea0a21c9d7bf70ea9b945588c1c66392578d1" "52588047a0fe3727e3cd8a90e76d7f078c9bd62c0b246324e557dfa5112e0d0c" "b97a01622103266c1a26a032567e02d920b2c697ff69d40b7d9956821ab666cc" "a24c5b3c12d147da6cef80938dca1223b7c7f70f2f382b26308eba014dc4833a" "732b807b0543855541743429c9979ebfb363e27ec91e82f463c91e68c772f6e3" default)))
  '(fci-rule-color "#dadada")
  '(hl-sexp-background-color "#efebe9")
  '(package-archives
@@ -31,7 +31,7 @@
      ("melpa-stable" . "http://stable.melpa.org/packages/"))))
  '(package-selected-packages
    (quote
-    (elpy material-theme dracula-theme markdown-mode neotree groovy-mode haskell-mode)))
+    (rainbow-delimiters projectile cider clojure-mode-extra-font-locking clojure-mode magit py-autopep8 flycheck elpy material-theme dracula-theme markdown-mode neotree groovy-mode haskell-mode)))
  '(vc-annotate-background nil)
  '(vc-annotate-color-map
    (quote
@@ -55,6 +55,7 @@
      (360 . "#558b2f"))))
  '(vc-annotate-very-old-color nil))
 (package-initialize)
+(elpy-enable)
 
 ;; refresh & install selected packages
 ; (package-refresh-contents)
@@ -74,3 +75,28 @@
 
 ;; remove all trailing whitespace
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
+
+(when (require 'flycheck nil t)
+  (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
+  (add-hook 'elpy-mode-hook 'flycheck-mode))
+
+(require 'py-autopep8)
+(add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save)
+
+(elpy-use-ipython "ipython3")
+
+(global-set-key (kbd "C-x g") 'magit-status)
+(global-magit-file-mode 1)
+(put 'erase-buffer 'disabled nil)
+
+(add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
+
+;; Color Themes
+;; Read http://batsov.com/articles/2012/02/19/color-theming-in-emacs-reloaded/
+;; for a great explanation of emacs color themes.
+;; https://www.gnu.org/software/emacs/manual/html_node/emacs/Custom-Themes.html
+;; for a more technical explanation.
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
+(add-to-list 'load-path "~/.emacs.d/themes")
+; (load-theme 'tomorrow-night-bright t)
+(load-theme 'material)
